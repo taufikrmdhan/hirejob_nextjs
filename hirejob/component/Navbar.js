dedeb6dc576260ded0a801f77ae30183140d1c0a
Taufik Ramadhan <taufik.30@students.amikom.ac.id>
2022-11-09 01:52:33 +0700

    notif not update

--- hirejob/component/Navbar.js
+++ hirejob/component/Navbar.js
@@ -1,12 +1,10 @@
-
-import React, {useEffect,useState} from "react";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 // import axios from "axios";
 // import {useRouter} from "next/router";
 
 const Navbar = () => {
-
   // const router = useRouter();
   // const [user, setUser] = useState({});
 
@@ -25,72 +23,81 @@ const Navbar = () => {
   // }, []);
 
   return (
-     <div className="container-fluid">
-        <nav className="navbar navbar-expand-lg bg-white">
-          <div className="container-fluid">
+    <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg bg-white">
+        <div className="container-fluid">
           <Link className="navbar-brand mx-5" href="/">
-            <Image
-              src="/Group 980 2.png"
-              width="100"
-              height={30}
-              alt="logo"
-            />
+            <Image src="/Group 980 2.png" width="100" height={30} alt="logo" />
           </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarText"
-              aria-controls="navbarText"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarText">
-              <ul className="navbar-nav me-auto mb-2 ms-5 mb-lg-0">
-                <li className="nav-item">
-                  <Link className="nav-link active" aria-current="page" href="/home/home">
-                    Home
-                  </Link>
-                </li>
-              </ul>
-              <div className="dropdown">
-                <a
-                  className="nav-link active mx-5"
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav me-auto mb-2 ms-5 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
                   aria-current="page"
-                  href="#"
-                  id="dropdownMenuButton1"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
+                  href="/home/home"
                 >
-                  <img
-                    src="/Group 797.png"
-                    width="20"
-                    alt=""
-                    className="mx-4"
-                  />
-                  <img
-                    src="/btnNotifications.png"
-                    width="20"
-                    alt=""
-                    className="ms-2"
-                  />
-                </a>
-              </div>
-              <Link href="/updateprofile/update"><Image
-                  src="/nnzkZNYWHaU.png"
-                  width={45}
-                  height={45}
-                  className="mobileProfileNavbar mx-5 rounded-circle customBorder"
-                  alt="profile image"
-                /></Link>
+                  Home
+                </Link>
+              </li>
+            </ul>
+            <div className="dropdown">
+              <a
+                className="nav-link active mx-5"
+                aria-current="page"
+                href="#"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img src="/Group 797.png" width="20" alt="" className="mx-4" />
+                <img
+                  src="/btnNotifications.png"
+                  width="20"
+                  alt=""
+                  className="ms-2"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseWidthExample"
+                  aria-expanded="false"
+                  aria-controls="collapseWidthExample"
+                />
+                <div>
+                  <div
+                    className="collapse collapse-vertical"
+                    id="collapseWidthExample"
+                  >
+                    <div className="card card-body" width={40}>
+                    <img src="/notify.png" width="20" alt="" className="mx-4" />
+                    </div>
+                  </div>
+                </div>
+              </a>
             </div>
+            <Link href="/updateprofile/update">
+              <Image
+                src="/nnzkZNYWHaU.png"
+                width={45}
+                height={45}
+                className="mobileProfileNavbar mx-5 rounded-circle customBorder"
+                alt="profile image"
+              />
+            </Link>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 export default Navbar;
-
-
