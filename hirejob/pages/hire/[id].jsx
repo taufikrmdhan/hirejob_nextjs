4f45b6006d06ba1fedf0afaf54b4723ca9d2cad4
Taufik Ramadhan <taufik.30@students.amikom.ac.id>
2022-12-13 13:26:11 +0700

    change ssg to ssr perusahaan && hire check id query is exist

--- hirejob/pages/hire/[id].jsx
+++ hirejob/pages/hire/[id].jsx
@@ -7,22 +7,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { protectedRoute } from "../../HOC/protectionRoute";
 
-const Index = (props) => {
-  const [data, setData] = useState(props.resUser[0]);
-  console.log(props.resUser)
-
-  // useEffect(() => {
-  //   axios
-  //     .get(`${process.env.NEXT_PUBLIC_API_URL}/user/list/${id}`)
-  //     .then((res) => {
-  //       console.log(res.data.data);
-  //       setData(res.data.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
-
+const Index = ({ user }) => {
   return (
     <>
       <div>
@@ -34,151 +19,127 @@ const Index = (props) => {
         <div className="">
           <div className="col-md-12 bg-light marginNegatif">
             <div className="container ">
-              {
-                data? (
-                  <div className="row">
-                {data.length === 0 ? (
-                  <p>Data is not available</p>
-                ) : (
-                  data.map((item) => {
-                    return (
-                      <div className="row">
-                        <div
-                          key={item.id_user}
-                          className="col-md-3 bg-white mt-5 mb-5 rounded p-2 px-4 "
-                        >
-                          <div className="col-md-12 my-2">
-                            <img
-                              src={`${process.env.NEXT_PUBLIC_API_URL}/${item.image}`}
-                              width="100"
-                              alt=""
-                              className="mx-auto d-block rounded-circle"
-                            />
-                          </div>
-                          <div className="col-md-12 my-2 mt-4">
-                            <h5>{item.name}</h5>
-                          </div>
-                          <div className="col-md-12 my-2 mt-2">
-                            <p>
-                              Lorem ipsum dolor sit, amet consectetur
-                              adipisicing elit. Earum.
-                            </p>
-                          </div>
-                          <div className="col-md-12 my-2 mt-2">
-                            <div className="row">
-                              <div className="col-md-1">
-                                <i className="fa fa-map-marker"></i>
-                              </div>
-                              <div className="col-md-10">
-                                <p className="text-muted">{item.city}</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="col-md-12 my-2 mt-2">
-                            <p className="text-muted">{item.description}</p>
-                          </div>
-
-                          <div className="col-md-12 mt-4">
-                            <h6>Skill</h6>
-                          </div>
-                          <div className="col-md-12">
-                            <button
-                              type="button"
-                              className="btn btnCustom ms-2 mt-2"
-                            >
-                              {item.skill}
-                            </button>
-                            <button
-                              type="button"
-                              className="btn btnCustom ms-2 mt-2"
-                            >
-                              {item.skill}
-                            </button>
-                          </div>
-                        </div>
-                        <div className="col-md-8 mt-5 mb-5 ms-4 rounded">
-                          <div className="col-md-12 my-2 mt-4">
-                            <h4>Hubungi {item.name}</h4>
-                          </div>
-                          <div className="col-md-12 my-2 mt-3">
-                            <p>
-                              Lorem ipsum dolor sit amet consectetur adipisicing
-                              elit. Quisquam, quae.
-                            </p>
-                          </div>
-                          <div className="col-md-12 my-2 mt-5">
-                            <label htmlFor="tujuan" className="text-muted mb-1">
-                              Tujuan pesan ini
-                            </label>
-                            <input
-                              type="text"
-                              className="form-control"
-                              id="tujuan"
-                              placeholder="Project"
-                            />
-                          </div>
-                          <div className="col-md-12 my-2 mt-5">
-                            <label htmlFor="nama" className="text-muted mb-1">
-                              Nama Lengkap
-                            </label>
-                            <input
-                              type="text"
-                              className="form-control"
-                              id="nama"
-                              placeholder="Louis Thomas"
-                            />
-                          </div>
-                          <div className="col-md-12 my-2 mt-5">
-                            <label htmlFor="email" className="text-muted mb-1">
-                              Email
-                            </label>
-                            <input
-                              type="text"
-                              className="form-control"
-                              id="email"
-                              placeholder="@gmail"
-                            />
-                          </div>
-                          <div className="col-md-12 my-2 mt-5">
-                            <label htmlFor="nomor" className="text-muted mb-1">
-                              No Handphone
-                            </label>
-                            <input
-                              type="text"
-                              className="form-control"
-                              id="nomor"
-                              placeholder="08123456789"
-                            />
-                          </div>
-                          <div className="col-md-12 my-2 mt-5">
-                            <label
-                              htmlFor="deskripsi"
-                              className="text-muted mb-1"
-                            >
-                              Deskripsi
-                            </label>
-                            <textarea
-                              className="form-control"
-                              id="deskripsi"
-                              rows="5"
-                              placeholder="Deskripsi jelaskan lebih detail"
-                            ></textarea>
-                          </div>
-                          <button type="button" className="btn btnAdd mt-5">
-                            Hire
-                          </button>
-                        </div>
+              <div className="row">
+                <div className="col-md-3 bg-white mt-5 mb-5 rounded p-2 px-4 ">
+                  <div className="col-md-12 my-2">
+                    <img
+                      src={`${process.env.NEXT_PUBLIC_API_URL}/${user.image}`}
+                      width="100"
+                      alt=""
+                      className="mx-auto d-block rounded-circle"
+                    />
+                  </div>
+                  {user.name ? (
+                    <div className="col-md-12 my-2 mt-4">
+                      <h5>{user.name}</h5>
+                    </div>
+                  ) : null}
+                  <div className="col-md-12 my-2 mt-2">
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Earum.
+                    </p>
+                  </div>
+                  {user.city ? (
+                  <div className="col-md-12 my-2 mt-2">
+                    <div className="row">
+                      <div className="col-md-1">
+                        <i className="fa fa-map-marker"></i>
                       </div>
-                    );
-                  })
-                )}
+                      <div className="col-md-10">
+                        <p className="text-muted">{user.city}</p>
+                      </div>
+                    </div>
+                  </div>
+                  ) : null}
+                  {user.description ? (
+                  <div className="col-md-12 my-2 mt-2">
+                    <p className="text-muted">{user.description}</p>
+                  </div>
+                  ) : null}
+
+                  <div className="col-md-12 mt-4">
+                    <h6>Skill</h6>
+                  </div>
+                  <div className="col-md-12">
+                    <button type="button" className="btn btnCustom ms-2 mt-2">
+                      {user.skill}
+                    </button>
+                    <button type="button" className="btn btnCustom ms-2 mt-2">
+                      {user.skill}
+                    </button>
+                  </div>
+                </div>
+                <div className="col-md-8 mt-5 mb-5 ms-4 rounded">
+                  <div className="col-md-12 my-2 mt-4">
+                    <h4>Hubungi {user.name || ''}</h4>
+                  </div>
+                  <div className="col-md-12 my-2 mt-3">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quisquam, quae.
+                    </p>
+                  </div>
+                  <div className="col-md-12 my-2 mt-5">
+                    <label htmlFor="tujuan" className="text-muted mb-1">
+                      Tujuan pesan ini
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="tujuan"
+                      placeholder="Project"
+                    />
+                  </div>
+                  <div className="col-md-12 my-2 mt-5">
+                    <label htmlFor="nama" className="text-muted mb-1">
+                      Nama Lengkap
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="nama"
+                      placeholder="Louis Thomas"
+                    />
+                  </div>
+                  <div className="col-md-12 my-2 mt-5">
+                    <label htmlFor="email" className="text-muted mb-1">
+                      Email
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="email"
+                      placeholder="@gmail"
+                    />
+                  </div>
+                  <div className="col-md-12 my-2 mt-5">
+                    <label htmlFor="nomor" className="text-muted mb-1">
+                      No Handphone
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="nomor"
+                      placeholder="08123456789"
+                    />
+                  </div>
+                  <div className="col-md-12 my-2 mt-5">
+                    <label htmlFor="deskripsi" className="text-muted mb-1">
+                      Deskripsi
+                    </label>
+                    <textarea
+                      className="form-control"
+                      id="deskripsi"
+                      rows="5"
+                      placeholder="Deskripsi jelaskan lebih detail"
+                    ></textarea>
+                  </div>
+                  <button type="button" className="btn btnAdd mt-5">
+                    Hire
+                  </button>
+                </div>
               </div>
-                )
-                : (
-                  <div>Data kosong</div>
-                )
-              }
-              
             </div>
           </div>
         </div>
@@ -189,29 +150,38 @@ const Index = (props) => {
 };
 Index.layout = "L";
 export async function getServerSideProps(context) {
-  const id = context.params.id;
-  let resUser = [];
+  const { id } = context.query;
 
-  try {
-    // const { data } = await axios.get(
-    //   `${process.env.NEXT_PUBLIC_API_URL}/user/list/${id}`
-    // );
-    // resUser.push(data.data);
-    const resultList = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/user/list/${id}`,
-      {
-        method: "GET",
-      }
-    );
-    const data = await resultList.json();
-    resUser.push(data.data);
-  } catch (err) {
-    console.log(err);
+  if (!id) {
+    return {
+      redirect: {
+        destination: "/home/home",
+        permanent: false,
+      },
+    };
   }
+
+  const resUser = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/user/list/${id}`,
+    { method: "GET" }
+  );
+
+  let { data: dataUser } = await resUser.json();
+
+  if (!dataUser) {
+    return {
+      redirect: {
+        destination: "/home/home",
+        permanent: false,
+      },
+    };
+  }
+
+  const user = Object.values(dataUser)[0];
 
   return {
     props: {
-      resUser,
+      user,
     },
   };
 }
--- hirejob/pages/perusahaan/[id].jsx
+++ hirejob/pages/perusahaan/[id].jsx
@@ -1,3 +1,154 @@
+// import React from "react";
+// import Head from "next/head";
+// import Footer from "../../component/Footer";
+// import Link from "next/link";
+// import axios from "axios";
+// import { protectedRoute } from "../../HOC/protectionRoute";
+
+// export async function getStaticProps(context) {
+//   try {
+//     const {id} = context.params;
+//     console.log(id)
+//     // const response = await axios({
+//     //     method: 'GET',
+//     //     url: `${process.env.NEXT_PUBLIC_API_URL}/recruiter/list/${id}`,
+//     // })
+//     const resultList = await fetch(
+//       `${process.env.NEXT_PUBLIC_API_URL}/recruiter/list/${id}`,
+//       {
+//         method: "GET",
+//       }
+//     )
+//     const data = await resultList.json();
+//     return {
+//         props: {
+//             data: data.data
+//         },
+//         revalidate: 1,
+//         notFound: false
+//     }
+//   }
+//   catch (err) {
+//     return {
+//         props: {
+//             data: null
+//         },
+//         revalidate: 1,
+//         notFound: true
+//     }
+//   }
+// }
+
+// export async function getStaticPaths(){
+//     const response = await axios({
+//         method: 'GET',
+//         url: `${process.env.NEXT_PUBLIC_API_URL}/recruiter/list`,
+//     })
+//     const data = response.data.data
+//     const paths = data.map((item) => ({
+//         params: {
+//             id: item.id_recruiter.toString()
+//         }
+//     }))
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
+
+// const Index = (props) => {
+//   return (
+//     <>
+//       <Head>
+//         <title>Profile perusahaan</title>
+//         <meta name="keywords" content="profile perusahaan" />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+//       <div className="container-fluid">
+//         {
+//         props.data.map((item) => {
+//           return (
+//             <div key={item.id_recruiter} className="row text-center p-2 m-5 bg-light shadow-lg rounded">
+//               <div className="col-md-12 bgUngu">
+//                 <button className="btn  d-flex justify-content-end align-items-end text-white">
+//                   &#9998; Ubah Latar
+//                 </button>
+//               </div>
+//               <div className="col-md-12 mb-3 upProfilePerusahaan">
+//                 <img
+//                   src={`${process.env.NEXT_PUBLIC_API_URL}/${item.image}`}
+//                   width={100}
+//                   height={100}
+//                   alt=""
+//                   className="rounded-circle border border-white"
+//                 />
+//               </div>
+//               <div className="col-md-12">
+//                 <h3>{item.company_name}</h3>
+//               </div>
+//               <div className="col-md-12">
+//                 <p>{item.business}</p>
+//               </div>
+//               <div className="col-md-12">
+//                 <p className="text-muted">{item.city}</p>
+//               </div>
+//               <div className="col-md-12 mt-3">
+//                 <p className="text-muted mx-5">{item.description}</p>
+//               </div>
+//               <div className="col-md-12 my-3">
+//                 <Link href="/updateperus/update">
+//                   <button type="button" className="btn btnGrape">
+//                     Edit profile
+//                   </button>
+//                 </Link>
+//               </div>
+//               <div className="col-md-12 mt-3 d-flex justify-content-center">
+//                 <div className="row">
+
+//                   <div className="col-md-1">
+//                     <i className="fa fa-envelope text-muted"></i>
+//                   </div>
+//                   <div className="col-md-8">
+//                     <p className="text-muted">{item.email}</p>
+
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="col-md-12  d-flex justify-content-center">
+//                 <div className="row">
+
+//                   <div className="col-md-1">
+//                     <i className="fa fa-github text-muted"></i>
+//                   </div>
+//                   <div className="col-md-8">
+//                     <p className="text-muted">{item.instagram}</p>
+
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="col-md-12 mb-5 d-flex justify-content-center">
+//                 <div className="row">
+
+//                   <div className="col-md-1">
+//                     <i className="fa fa-linkedin text-muted"></i>
+//                   </div>
+//                   <div className="col-md-8">
+//                     <p className="text-muted">{item.linkedin}</p>
+
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           );
+//         })}
+//       </div>
+//       <Footer />
+//     </>
+//   );
+// };
+// Index.layout = "L";
+// export default protectedRoute(Index);
+
 import React from "react";
 import Head from "next/head";
 import Footer from "../../component/Footer";
@@ -5,58 +156,7 @@ import Link from "next/link";
 import axios from "axios";
 import { protectedRoute } from "../../HOC/protectionRoute";
 
-export async function getStaticProps(context) {
-  try {
-    const {id} = context.params;
-    console.log(id)
-    // const response = await axios({
-    //     method: 'GET',
-    //     url: `${process.env.NEXT_PUBLIC_API_URL}/recruiter/list/${id}`,
-    // })
-    const resultList = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/recruiter/list/${id}`,
-      {
-        method: "GET",
-      }
-    )
-    const data = await resultList.json();
-    return {
-        props: {
-            data: data.data
-        },
-        revalidate: 1,
-        notFound: false
-    }
-  } 
-  catch (err) {
-    return {
-        props: {
-            data: null
-        },
-        revalidate: 1,
-        notFound: true
-    }
-  }
-}
-
-export async function getStaticPaths(){
-    const response = await axios({
-        method: 'GET',
-        url: `${process.env.NEXT_PUBLIC_API_URL}/recruiter/list`,
-    })
-    const data = response.data.data
-    const paths = data.map((item) => ({
-        params: {
-            id: item.id_recruiter.toString()
-        }
-    }))
-    return {
-        paths,
-        fallback: false
-    }
-}
-
-const Index = (props) => {
+const Index = ({ perusahaan }) => {
   return (
     <>
       <Head>
@@ -65,86 +165,121 @@ const Index = (props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="container-fluid">
-        {
-        props.data.map((item) => {
-          return (
-            <div key={item.id_recruiter} className="row text-center p-2 m-5 bg-light shadow-lg rounded">
-              <div className="col-md-12 bgUngu">
-                <button className="btn  d-flex justify-content-end align-items-end text-white">
-                  &#9998; Ubah Latar
-                </button>
+        <div className="row text-center p-2 m-5 bg-light shadow-lg rounded">
+          <div className="col-md-12 bgUngu">
+            <button className="btn  d-flex justify-content-end align-items-end text-white">
+              &#9998; Ubah Latar
+            </button>
+          </div>
+          <div className="col-md-12 mb-3 upProfilePerusahaan">
+            <img
+              src={`${process.env.NEXT_PUBLIC_API_URL}/${perusahaan.image}`}
+              width={100}
+              height={100}
+              alt=""
+              className="rounded-circle border border-white"
+            />
+          </div>
+          {perusahaan.company_name ? (
+          <div className="col-md-12">
+            <h3>{perusahaan.company_name}</h3>
+          </div>
+          ) : null}
+          {perusahaan.business ? (
+          <div className="col-md-12">
+            <p>{perusahaan.business}</p>
+          </div>
+          ) : null}
+          {perusahaan.city ? (
+          <div className="col-md-12">
+            <p className="text-muted">{perusahaan.city}</p>
+          </div>
+          ) : null}
+          {perusahaan.description ? (
+          <div className="col-md-12 mt-3">
+            <p className="text-muted mx-5">{perusahaan.description}</p>
+          </div>
+          ) : null}
+          <div className="col-md-12 my-3">
+            <Link href="/updateperus/update">
+              <button type="button" className="btn btnGrape">
+                Edit profile
+              </button>
+            </Link>
+          </div>
+          <div className="col-md-12 mt-3 d-flex justify-content-center">
+            <div className="row">
+              <div className="col-md-1">
+                <i className="fa fa-envelope text-muted"></i>
               </div>
-              <div className="col-md-12 mb-3 upProfilePerusahaan">
-                <img
-                  src={`${process.env.NEXT_PUBLIC_API_URL}/${item.image}`}
-                  width={100}
-                  height={100}
-                  alt=""
-                  className="rounded-circle border border-white"
-                />
-              </div>
-              <div className="col-md-12">
-                <h3>{item.company_name}</h3>
-              </div>
-              <div className="col-md-12">
-                <p>{item.business}</p>
-              </div>
-              <div className="col-md-12">
-                <p className="text-muted">{item.city}</p>
-              </div>
-              <div className="col-md-12 mt-3">
-                <p className="text-muted mx-5">{item.description}</p>
-              </div>
-              <div className="col-md-12 my-3">
-                <Link href="/updateperus/update">
-                  <button type="button" className="btn btnGrape">
-                    Edit profile
-                  </button>
-                </Link>
-              </div>
-              <div className="col-md-12 mt-3 d-flex justify-content-center">
-                <div className="row">
-                  
-                  <div className="col-md-1">
-                    <i className="fa fa-envelope text-muted"></i>
-                  </div>
-                  <div className="col-md-8">
-                    <p className="text-muted">{item.email}</p>
-                    
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-12  d-flex justify-content-center">
-                <div className="row">
-                  
-                  <div className="col-md-1">
-                    <i className="fa fa-github text-muted"></i>
-                  </div>
-                  <div className="col-md-8">
-                    <p className="text-muted">{item.instagram}</p>
-                    
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-12 mb-5 d-flex justify-content-center">
-                <div className="row">
-                  
-                  <div className="col-md-1">
-                    <i className="fa fa-linkedin text-muted"></i>
-                  </div>
-                  <div className="col-md-8">
-                    <p className="text-muted">{item.linkedin}</p>
-                    
-                  </div>
-                </div>
+              <div className="col-md-8">
+                <p className="text-muted">{perusahaan.email}</p>
               </div>
             </div>
-          );
-        })}
+          </div>
+          <div className="col-md-12  d-flex justify-content-center">
+            <div className="row">
+              <div className="col-md-1">
+                <i className="fa fa-github text-muted"></i>
+              </div>
+              <div className="col-md-8">
+                <p className="text-muted">{perusahaan.instagram}</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-12 mb-5 d-flex justify-content-center">
+            <div className="row">
+              <div className="col-md-1">
+                <i className="fa fa-linkedin text-muted"></i>
+              </div>
+              <div className="col-md-8">
+                <p className="text-muted">{perusahaan.linkedin}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
   );
 };
+
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+
+  if (!id) {
+    return {
+      redirect: {
+        destination: "/home/homeRec",
+        permanent: false,
+      },
+    };
+  }
+
+  const resRecruiter = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/recruiter/list/${id}`,
+    { method: "GET" }
+  );
+
+  let { data: dataRecruiter } = await resRecruiter.json();
+
+  if (!dataRecruiter) {
+    return {
+      redirect: {
+        destination: "/home/homeRec",
+        permanent: false,
+      },
+    };
+  }
+
+  const perusahaan = Object.values(dataRecruiter)[0];
+
+  return {
+    props: {
+      perusahaan,
+    },
+  };
+}
+
 Index.layout = "L";
 export default protectedRoute(Index);
